1bad71722d6e0af5fecfb7d61799b3dc8dcc8012
wott <william.ott@medicustek.com>
2016-07-22 11:52:41 +0800

    Beautify the ugly part

--- mtemail.py
+++ mtemail.py
@@ -1,5 +1,6 @@
 from email.mime.text import MIMEText
-from error import Error, AuthenticationError, InternalError, MissingAttributeError
+from error import Error, AuthenticationError, InternalError, MissingAttributeError, \
+        UnknownError
 from logging import getLogger
 import smtplib
 
@@ -42,29 +43,30 @@ class EmailNotificationService(object):
             else:
                 s = smtplib.SMTP(host=settings['server'], port=settings['port'],
                         timeout=self.timeout)
-        except smtplib.SMTPConnectError as e:
-            LOGGER.error(str(e))
-            raise InternalError('Failed to connect to SMTP server')
 
-        try:
             if settings['auth'] and ('user' in settings and 'password' in settings):
                 s.login(settings['user'], settings['password'])
                 raise MissingAttributeError('No user/password supplied')
+
+
+            s.sendmail(settings['fromAddr'], settings['toAddr'], msg.as_string())
+        except smtplib.SMTPConnectError as e:
+            LOGGER.error(str(e))
+            raise InternalError('Failed to connect to SMTP server')
         except smtplib.SMTPAuthenticationError as e:
-            s.quit()
             LOGGER.warning(str(e))
             raise AuthenticationError('Failed to authenticate with SMTP server')
-        except Error as e:
-            s.quit()
-            LOGGER.warning(str(e))
-            raise e
-
-        try:
-            s.sendmail(settings['fromAddr'], settings['toAddr'], msg.as_string())
         except smtplib.SMTPException as e:
             LOGGER.error(str(e))
             raise InternalError('Something went wrong with sending the email')
+        except Error as e:
+            LOGGER.warning(str(e))
+            raise e
+        except Exception as e:
+            LOGGER.error(str(e))
+            raise UnkownError('Woops, don\'t know what happened :/.')
         else:
             return True
         finally:
             s.quit()
+
