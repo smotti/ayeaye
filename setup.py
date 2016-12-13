825448d9f6fe21e74946baa8964581183916c07d
wott <william.ott@medicustek.com>
2016-12-13 15:33:35 +0800

    Add script and systemd timer & service to purge old notifications

--- setup.py
+++ setup.py
@@ -10,8 +10,10 @@ setup(name='ayeaye',
     package_dir={'ayeaye': 'ayeaye'},
     package_data={'ayeaye': ['schema.sql']},
     data_files=[('/usr/bin', ['ayeaye/ayeaye']),
+                ('/usr/bin', ['resources/ayeaye-purge']),
                 ('/etc/systemd/system', ['resources/ayeaye.service']),
                 ('/etc/systemd/system/ayeaye.service.d', ['resources/ayeaye.conf']),
+                ('/etc/systemd/system', ['resources/ayeaye-purge-notifications.service']),
+                ('/etc/systemd/system', ['resources/ayeaye-purge-notifications.timer']),
                 ('/var/lib/medicustek/ayeaye', ['requirements.txt'])]
-#    install_requires=['arrow', 'flask', 'flask-cors']
     )
